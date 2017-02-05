15d7ccc8cd98a4a7805372306b4bf9083d2c2123
Jason <jason@family-iMac.local>
2017-02-05 16:13:13 -0600

    Initial Commit

---
+++ Tick Tack ToeTests/Tick_Tack_ToeTests.swift
@@ -0,0 +1,36 @@
+//
+//  Tick_Tack_ToeTests.swift
+//  Tick Tack ToeTests
+//
+//  Created by Jason on 2/5/17.
+//  Copyright © 2017 Jason Mendez. All rights reserved.
+//
+
+import XCTest
+@testable import Tick_Tack_Toe
+
+class Tick_Tack_ToeTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
