05837f22e00d098d0fc78a087252eecf6d535e6e
Jason <jason@family-iMac.local>
2017-02-05 23:01:24 -0600

    Initial commit

--- Tick Tack Toe/ViewController.swift
+++ Tick Tack Toe/ViewController.swift
@@ -9,10 +9,59 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    var TicTacToe = 1 // cross
+    var table = [0,0,0,0,0,0,0,0,0]
+    
+    let WaysToWin = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8]]
+    var GameOn = true
+    
+    
+    @IBAction func ButtonAction(_ sender: UIButton)
+    {
+        if (table[sender.tag-1]==0)
+        {
+            if (TicTacToe == 1)
+            {
+                sender.setImage(UIImage(named:"X.png"), for: UIControlState())
+            }
+            else
+            {
+                sender.setImage(UIImage(named:"O.png"), for: UIControlState())
+            }
+            }
+        }
+    
+    
+
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        for combination in WaysToWin
+        {
+            if table[combination[0]] != 0 && table[combination[0]] == table[combination[1]] && table[combination[1]] == table[combination[2]]
+            {
+                GameOn = false
+                if table[combination[0]] == 1
+                {
+                    // X has won
+                    print ("X")
+                }
+                else
+                {
+                    //O has won
+                    print ("O");
+                }
+            }
+        }
+        
+        
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
